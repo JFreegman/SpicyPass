b1f2d87c14ba8d8529ca9fa0add0f075d7687dfd
jfreegman <jfreegman@gmail.com>
2025-07-08 14:28:55 -0400

    Update license stubs
    SpicyPass is now licensed under GPL v3.0-only

--- src/password.hpp
+++ src/password.hpp
@@ -1,23 +1,9 @@
 /*  password.hpp
  *
+ *  Copyright (C) 2020-2025 Jfreegman <Jfreegman@gmail.com>
  *
- *  Copyright (C) 2020-2024 Jfreegman <Jfreegman@gmail.com>
- *
- *  This file is part of SpicyPass.
- *
- *  SpicyPass is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  SpicyPass is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with SpicyPass.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *  This file is part of SpicyPass. SpicyPass is free software licensed
+ *  under the GNU General Public License 3.0.
  */
 
 #ifndef PASSWORD_H
